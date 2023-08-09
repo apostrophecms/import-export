fc5fcb07c3bac9c1c2695cb5a46d9deda3f3ccb3
Etienne Laurent <etldev@tuta.io>
2023-08-09 16:38:07 +0200

    display import modal

--- modules/@apostrophecms/import-export-piece-type/index.js
+++ modules/@apostrophecms/import-export-piece-type/index.js
@@ -3,6 +3,34 @@ module.exports = {
 
   cascades: [ 'batchOperations' ],
 
+  utilityOperations (self) {
+    return self.options.import !== false
+      ? {
+        add: {
+          import: {
+            label: 'aposImportExport:import',
+            modalOptions: {
+              title: 'aposImportExport:importType',
+              descriptionStart: 'aposImportExport:importModalDescriptionStart',
+              descriptionLink: 'aposImportExport:importModalDescriptionLink',
+              descriptionEnd: 'aposImportExport:importModalDescriptionEnd',
+              confirmationButton: 'aposImportExport:importType',
+
+              // TODO: rename AposImportPiecesModal to AposImportModal since pages can be imported, not only pieces
+              modal: 'AposImportPiecesModal'
+            },
+            messages: {
+              progress: 'Importing {{ type }}...'
+            },
+            requestOptions: {
+              extension: 'zip'
+            }
+          }
+        }
+      }
+      : {};
+  },
+
   batchOperations(self) {
     if (self.options.export === false) {
       return;
