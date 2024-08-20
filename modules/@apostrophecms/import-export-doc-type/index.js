1154009c7ddfa1ad5e7684809e47883601e5e72a
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2024-08-20 18:41:07 +0300

    Revert singleton context menu

--- modules/@apostrophecms/import-export-doc-type/index.js
+++ modules/@apostrophecms/import-export-doc-type/index.js
@@ -27,27 +27,5 @@ module.exports = {
     }
 
     self.apos.doc.addContextOperation(operation);
-
-    // Add import operation for singleton doc types
-    if (self.options.singleton === true || !!self.options.singletonAuto) {
-      if (self.options.importExport?.import === false) {
-        return;
-      }
-      console.log('self.options.singleton', self.name, (self.options.singleton === true || !!self.options.singletonAuto), (self.options.singleton === true || self.options.singletonAuto));
-      const importOperation = {
-        action: 'import-export-import',
-        context: 'update',
-        label: 'aposImportExport:import',
-        modal: 'AposImportModal',
-        props: {
-          action: 'import-export-import'
-        }
-      };
-      if (excludedTypes.length) {
-        operation.if = { $and: excludedTypes };
-      }
-
-      self.apos.doc.addContextOperation(importOperation);
-    }
   }
 };
