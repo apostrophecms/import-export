6002adee79b0cefbf72ac63525d12642b0a20eba
Jed <vjeudy@protonmail.com>
2024-08-26 15:05:48 +0200

    moves handler in lib file, only checks canEdit for singleton import (cannot be created)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const handlers = require('./lib/handlers');
 const methods = require('./lib/methods');
 const apiRoutes = require('./lib/apiRoutes');
 const formats = require('./lib/formats');
@@ -28,60 +29,7 @@ module.exports = {
 
     self.enableBrowserData();
   },
-  handlers(self) {
-    return {
-      'apostrophe:modulesRegistered': {
-        setContextOperations() {
-          const excludedExportTypes = [];
-          const excludedImportTypes = [];
-          for (const mod of Object.values(self.apos.modules)) {
-            if (!self.apos.instanceOf(mod, '@apostrophecms/doc-type')) {
-              continue;
-            }
-
-            if (mod.options.importExport?.export === false) {
-              excludedExportTypes.push({
-                type: {
-                  $ne: mod.__meta.name
-                }
-              });
-            }
-
-            if (!mod.options.singleton || mod.options.importExport?.import === false) {
-              excludedImportTypes.push({
-                type: {
-                  $ne: mod.__meta.name
-                }
-              });
-            }
-          }
-
-          const exportOperation = {
-            action: 'import-export-export',
-            context: 'update',
-            label: 'aposImportExport:export',
-            modal: 'AposExportModal',
-            props: {
-              action: 'import-export-export'
-            },
-            if: { $and: excludedExportTypes }
-          };
-          const importOperation = {
-            action: 'import-export-import',
-            context: 'update',
-            label: 'aposImportExport:import',
-            modal: 'AposImportModal',
-            if: { $and: excludedImportTypes },
-            canCreate: true,
-            canEdit: true
-          };
-
-          self.apos.doc.addContextOperation(exportOperation);
-          self.apos.doc.addContextOperation(importOperation);
-        }
-      }
-    };
-  },
+  handlers,
   methods,
   apiRoutes
 };
